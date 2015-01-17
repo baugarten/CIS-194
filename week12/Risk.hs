fc13f07fe9c190673ee3d3890e826f93ea1be70b
Ben Augarten <baugarten@twitter.com>
2015-01-17 01:18:36 -0700

    Initial commit of week12

---
+++ week12/Risk.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module Risk where
+
+import Control.Monad
+import Control.Monad.Random
+import Data.List
+import Debug.Trace
+
+------------------------------------------------------------
+-- Die values
+
+newtype DieValue = DV { unDV :: Int } 
+  deriving (Eq, Ord, Show, Num)
+
+first :: (a -> b) -> (a, c) -> (b, c)
+first f (a, c) = (f a, c)
+
+instance Random DieValue where
+  random           = first DV . randomR (1,6)
+  randomR (low,hi) = first DV . randomR (max 1 (unDV low), min 6 (unDV hi))
+
+die :: Rand StdGen DieValue
+die = getRandom
+
+------------------------------------------------------------
+-- Risk
+
+type Army = Int
+
+data Battlefield = Battlefield { attackers :: Army, defenders :: Army } deriving (Show)
+
+-- Exercise 2
+
+battle :: Battlefield -> Rand StdGen Battlefield
+battle bf@(Battlefield numAttackers numDefenders)
+  | numAttackers < 2 || numDefenders < 1 = return bf
+  | otherwise = do
+      ds <- replicateM maxDefenders die
+      as <- replicateM maxAttackers die
+      return $ newBattle bf (sortDesc as) (sortDesc ds)
+    where maxAttackers = min 3 (numAttackers - 1)
+          maxDefenders = min 2 numDefenders
+          sortDesc = sortBy (flip compare)
+
+newBattle :: Battlefield -> [DieValue] -> [DieValue] -> Battlefield
+newBattle bf [] _ = bf
+newBattle bf _ [] = bf
+newBattle bf (a:as) (d:ds)
+  | d >= a = newBattle (defended bf) as ds
+  | otherwise = newBattle (attacked bf) as ds
+  where
+    defended (Battlefield na nd) = Battlefield (na - 1) nd
+    attacked (Battlefield na nd) = Battlefield na (nd - 1)
+
+-- Exercise 3
+
+invade :: Battlefield -> Rand StdGen Battlefield
+invade bf@(Battlefield 0 _) = return bf
+invade bf@(Battlefield _ 0) = return bf
+invade bf = battle bf >>= invade
+
+-- Exercise 4
+num = 100
+successProb :: Battlefield -> Rand StdGen Double
+successProb bf = do
+  results <- replicateM num (invade bf)
+  return $ fromIntegral (wins results) / fromIntegral num
+  where wins = length . filter ((>0) . attackers)
